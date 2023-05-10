3e66d5305a72f9839cfe95cdb5048861a2b334b5
NunoLima10 <84800684+NunoLima10@users.noreply.github.com>
2023-05-10 21:15:07 -0100

    feat: api parse

---
+++ src/api/api.js
@@ -0,0 +1,34 @@
+import axios from "axios";
+
+
+const PedradaAPI = axios.create({
+    baseURL: "http://127.0.0.1:5000"
+})
+
+export async function parseAPIResponse(APIPromise) {
+    let data = null
+    let successMessage = null
+    let errorMessage = null
+
+    try {
+        const response = await APIPromise
+        const responseData = response.data
+        data = responseData.data
+        successMessage = responseData.success_message["0"]["1"]
+    } catch (error) {
+        const response = error.response
+        const responseData = await response.data
+        errorMessage = responseData.error_message["0"]["1"]
+    }
+    const APIResponse = {
+        data: data,
+        successMessage: successMessage,
+        errorMessage: errorMessage
+    }
+
+   return APIResponse
+}
+
+export default PedradaAPI;
+ // const message = dataResponse.success_message
+    // console.log(message["0"]["1"])
